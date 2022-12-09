3ceb0833a6f4538cb6c18bfed91d872ba7ab62d5
geneus003 <geneus003@gmail.com>
2022-12-09 23:02:33 +0300

    Nothing works, created only unrelated functions for future merge

--- main_temp.py
+++ main_temp.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import random
 
 
-def generate_model(structure_size, center, layers_sizes, layers_p):
+def generate_model(structure_size, center, layers_sizes, layers_p, sizes_limit_height, sizes_limit_width):
     def validate_data():
         if not(isinstance(structure_size, list) and isinstance(center, list) and isinstance(layers_sizes, list) and isinstance(layers_p, list)):
+            return False
+        if not(isinstance(sizes_limit_height, list) and isinstance(sizes_limit_width, list)):
             return False
         if len(structure_size) != 2 or len(center) != 2 or len(layers_sizes) + 1 != len(layers_p):
             return False
@@ -19,68 +22,65 @@ def generate_model(structure_size, center, layers_sizes, layers_p):
 
     structure = np.zeros(structure_size)
 
-    structure[center[0]][center[1]] = layers_p[-1]
+    for i, e in enumerate(structure):
+        for j in range(len(e)):
+            diff_y = abs(i - center[0])
+            diff_x = abs(j - center[1])
 
-    false_detections = []
+            dist = math.sqrt(diff_x ** 2 + diff_y ** 2)
 
-    def p_layer_recur(n_center, r, v):
-        cells = []
+            is_found = False
+            for k, c in enumerate(layers_sizes):
+                if dist < c:
+                    is_found = True
+                    structure[i][j] = layers_p[k]
+                    break
+            if not is_found:
+                structure[i][j] = layers_p[-1]
 
-        # print(n_center)
+    sizes_width = np.array([random.randint(sizes_limit_width[0], sizes_limit_width[1]) for i in range(structure_size[0])])
+    sizes_height = np.array([random.randint(sizes_limit_height[0], sizes_limit_height[1]) for i in range(structure_size[1])])
 
-        for i in range(n_center[0]-1, n_center[0]+2):
-            for j in range(n_center[1]-1, n_center[1]+2):
-                cells.append([i, j])
-
-        con = 0
-        for i, e in enumerate(cells):
-            try:
-                temp = structure[e[0]][e[1]]
-            except IndexError:
-                con += 1
-                continue
-
-            if structure[e[0]][e[1]] != 0:
-                con += 1
-                continue
-
-            diff_y = abs(e[0] - center[0])
-            diff_x = abs(e[1] - center[1])
-
-            dist = math.sqrt(diff_x**2 + diff_y**2)
-
-            if dist < r + 1:
-                structure[e[0]][e[1]] = v
-                # print("here", n_center, dist, r+1, cells)
-                tt = p_layer_recur([e[0], e[1]], r, v)
-                if tt[0]:
-                    false_detections.append(tt[1])
-            else:
-                con += 1
-        # print(con)
-        if con != 9:
-            return True, [n_center[0], n_center[1]]
-        else:
-            return False, [n_center[0], n_center[1]]
+    return structure, sizes_width, sizes_height
 
 
-    yy = p_layer_recur(center, layers_sizes[0], layers_p[0])
+def solve_model(structure, heights):
+    N = len(structure)
+    M = 5
+    T1 = 0.01
+    Q = 2
 
-    if yy[0]:
-        false_detections.append(yy[1])
-    new_false_detections = []
-    for i in false_detections:
-        if i not in new_false_detections:
-            new_false_detections.append(i)
+    rho_arr = structure
+    h_arr = heights
 
-    for i in new_false_detections:
-        structure[i[0]][i[1]] = 400
+    T_arr = np.array([T1 * Q ** i for i in range(M)])
+    Omega_arr = (2 * np.pi / np.asarray(T_arr))
+    mu = 4 * np.pi * 1e-07
+    R = np.empty(M, dtype=complex)
+    for i in range(M):
+        Rc = 1
+        for m in (range(N - 1)):
+            K = np.sqrt((-1j * Omega_arr[i] * mu) / (rho_arr[m]))
+            A = np.sqrt(rho_arr[m] / rho_arr[m + 1])
+            B = (np.exp(-2 * K * h_arr[m])) * ((Rc - A) / (Rc + A))
+            Rc = (1 + B) / (1 - B)
+        R[i] = (Rc)
+    rhok_arr = np.empty(M, dtype=float)
+    rhok_arr = np.round(((np.abs(R) ** 2) * rho_arr[0]), 2)
+    T_arr1 = np.sqrt(T_arr)
+    PhT = np.round(np.angle(R) * (180 / np.pi) - 45, 2)
+
+    return rhok_arr, PhT, T_arr1
+
+
+def main():
+    structure, structure_heights, structure_widths = generate_model([100, 100], [50, 50], [10, 30], [1000, 500, 100], [10, 10], [10, 10])
 
     figure, axs = plt.subplots(1, 1, figsize=(5, 5))
     p2 = axs.imshow(structure, cmap='jet', aspect='auto', interpolation='bilinear', origin="upper")
     figure.colorbar(p2)
-    figure.show()
+    plt.show()
 
-generate_model([20, 100], [10, 50], [2, 2], [1000, 500, 100])
 
-# structure_size (rows, columns)
+if __name__ == "__main__":
+    main()
